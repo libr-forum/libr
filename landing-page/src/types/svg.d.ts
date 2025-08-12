fa91c33aa1087fb99a3e5f2b0526e35d120d187b
Arnav-Agrawal-987 <arnav.agrawal.2609@gmail.com>
2025-08-12 19:23:27 +0530

    fix:removed older db and landing-page

--- landing-page/src/types/svg.d.ts
+++
@@ -1,6 +0,0 @@
-// For Vite + SVGR or similar setups
-declare module '*.svg?react' {
-  import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
